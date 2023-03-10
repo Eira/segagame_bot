10c3c50a404ba0bce0112bae7d392e37c7468e19
Irina <eira@mail.ru>
2023-03-10 23:09:01 +0100

    test added

---
+++ tests/test_buy_token.py
@@ -0,0 +1,13 @@
+from unittest.mock import AsyncMock
+
+from app.common_handlers import buy_token
+
+
+async def test_buy_token_smoke(mocker):
+    mock = mocker.patch('app.common_handlers.bot.send_photo')
+    message_mock = AsyncMock()
+
+    await buy_token(message_mock)
+
+    assert True
+    assert mock.call_count == 1
